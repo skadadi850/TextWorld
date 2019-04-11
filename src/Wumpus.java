9cd4d730d5ede20aa90695293e0b77087262f64c
Simran Kadadi <simrandkk@gmail.com>
2019-04-10 21:25:45 -0700

    create wumpus move method

--- src/Wumpus.java
+++ src/Wumpus.java
@@ -1,5 +1,5 @@
 public class Wumpus extends Creature{
-    private static Level.Room playerRoom;
+    private Level.Room playerRoom;
 
     public Wumpus (Level.Room wumpusRoom, Level.Room playerRoom, String name){
         super (wumpusRoom);
@@ -8,12 +8,13 @@ public class Wumpus extends Creature{
     @Override
     public void move() {
         if (playerRoom.areNeighbors(currentRoom)){
-
+            Level.Room next = currentRoom.getRandomRoom(currentRoom);
+            if (next.equals(playerRoom)){
+                currentRoom.neighbors.remove(next);
+                next = currentRoom.getRandomRoom(currentRoom);
+            }
+            moveToRoom (next);
         }
-
-
-        Level.Room next = currentRoom.getRandomRoom(currentRoom);
-        moveToRoom (next);
     }
 
 
